a76caf15f3c93298996a75f17dcdb7fc40d66913
Markus Kauppila <markus.kauppila@gmail.com>
2013-12-25 14:55:13 +0200

    Generate random spawn point for insects
    The position will on the right/left side of the game area.

--- Summer/InsectSpawner.m
+++ Summer/InsectSpawner.m
@@ -10,15 +10,39 @@
 
 #import "Insect.h"
 
+@interface InsectSpawner ()
+@property (assign, nonatomic) NSUInteger spawnCounter;
+@end
+
 @implementation InsectSpawner
+
+- (instancetype)init
+{
+	self = [super init];
+	if (self) {
+		self.spawnCounter = 0;
+	}
+	return self;
+}
 
 - (Insect *)spawn
 {
-	CGPoint position = CGPointMake(random() % 1000, random() % 1000);
-
+	self.spawnCounter++;
+	
+	CGPoint position = [self generateSpawnPosition];
 	Insect *insect = [[Insect alloc] initWithColor:[SKColor purpleColor] position:position];
 	return insect;
 }
 
+
+- (CGPoint)generateSpawnPosition
+{
+	bool spawnRight = self.spawnCounter % 2 == 0 ? YES : NO;
+	
+	CGFloat x = spawnRight ? 1200 : -100;
+	CGFloat y = random() % 768;
+	
+	return CGPointMake(x, y);
+}
 
 @end
