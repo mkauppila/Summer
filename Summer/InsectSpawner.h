8985fc77089529956763cbac4e651f5bb0123134
Markus Kauppila <markus.kauppila@gmail.com>
2013-12-25 12:04:16 +0200

    Add InsectSpawner
    Factory for creating the insects.

---
+++ Summer/InsectSpawner.h
@@ -0,0 +1,13 @@
+//
+//  InsectSpawner.h
+//  Summer
+//
+//  Created by markus on 25.12.2013.
+//  Copyright (c) 2013 Markus Kauppila. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface InsectSpawner : NSObject
+
+@end
---
+++ Summer/InsectSpawner.m
@@ -0,0 +1,13 @@
+//
+//  InsectSpawner.m
+//  Summer
+//
+//  Created by markus on 25.12.2013.
+//  Copyright (c) 2013 Markus Kauppila. All rights reserved.
+//
+
+#import "InsectSpawner.h"
+
+@implementation InsectSpawner
+
+@end
