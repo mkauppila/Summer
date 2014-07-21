89b9da74d3e65e2f50c2993c10b0f2c2657f4c4b
Markus Kauppila <markus.kauppila@gmail.com>
2014-07-21 22:59:09 +0300

    Add Heart model

---
+++ Summer/Heart.h
@@ -0,0 +1,13 @@
+//
+//  Heart.h
+//  Summer
+//
+//  Created by Markus Kauppila on 21/07/14.
+//  Copyright (c) 2014 Markus Kauppila. All rights reserved.
+//
+
+#import "GameObject.h"
+
+@interface Heart : GameObject
+
+@end
---
+++ Summer/Heart.m
@@ -0,0 +1,13 @@
+//
+//  Heart.m
+//  Summer
+//
+//  Created by Markus Kauppila on 21/07/14.
+//  Copyright (c) 2014 Markus Kauppila. All rights reserved.
+//
+
+#import "Heart.h"
+
+@implementation Heart
+
+@end
